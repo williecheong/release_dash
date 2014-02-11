dcb522dbf4ec6c3128c7bbb2e071adaed8782a61
williecheong <cheongwillie@hotmail.com>
2014-02-11 14:34:09 -0800

    DONE
    Groups of products are placed in the very beginning and also shaded
    with a very light grey.

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ release_dash
 
 Dashboard for tracking Release Statuses of Mozillian Awesomeness.
 
-Check the progress updates and latest developments on <a href="https://wiki.mozilla.org/User:Wcheong">this wiki.</a>
+Check the progress updates and latest developments on <a href="https://docs.google.com/spreadsheet/ccc?key=0ApNDjYXWm5JndDFwLWVlM1BPR3dBdjE1ZVdfWlBwR1E&usp=sharing">this spreadsheet.</a>
 
 
 
--- application/controllers/watch.php
+++ application/controllers/watch.php
@@ -54,47 +54,54 @@ class Watch extends CI_Controller {
         $data['title'] = $version->title;
         $data['query_groups'] = array();
 
-        /******************************************/
-        // Retrieving groups and queries by product
+        /********************************************
+            Retrieving groups and queries by product
+        *********************************************/
         $by_product = array( 
             'entity'    => 'product',
-            'entity_id' => $version->id );
+            'entity_id' => $product->id );
         $groups_by_product = $this->group->retrieve( $by_product );
         
         foreach ( $groups_by_product as $group ) {
-            $data['query_groups'][$group->tag]['title'] = $group->title;
-            $data['query_groups'][$group->tag]['group_id']  = $group->id;
-            $data['query_groups'][$group->tag]['is_plot']  = $group->is_plot;
-            $data['query_groups'][$group->tag]['is_number'] = $group->is_number;
-            $data['query_groups'][$group->tag]['is_rule'] = '1';
-            $data['query_groups'][$group->tag]['queries'] = array();    
+            $group_tag = replace_version_attr( $group->tag, $version );
+            $group_title = replace_version_attr( $group->title, $version );
+
+            $data['query_groups'][$group_tag]['title'] = $group_title;
+            $data['query_groups'][$group_tag]['group_id']  = $group->id;
+            $data['query_groups'][$group_tag]['is_plot']  = $group->is_plot;
+            $data['query_groups'][$group_tag]['is_number'] = $group->is_number;
+            $data['query_groups'][$group_tag]['is_rule'] = '1';
+            $data['query_groups'][$group_tag]['queries'] = array();    
 
             // Retrieve the stored Qb queries in this group.
             $by_group = array( 'group_id' => $group->id );
             $queries = $this->query->retrieve( $by_group );
 
             foreach ( $queries as $query ) {
+                $query_tag = replace_version_attr( $query->tag, $version );
+                $query_title = replace_version_attr( $query->title, $product, $version );
+
                 // Replace soft timestamps with current timestamp and birthday
                 $transformed_query = $query->query_qb;
-                
-                $shipday = $this->version->get_shipday( $version->id );
+
+                $transformed_query = replace_version_attr( $transformed_query, $version );
+                    $birthday = $this->version->get_birthday( $version->id );
+                $transformed_query = replace_birthday( $transformed_query, $birthday );
+                    $shipday = $this->version->get_shipday( $version->id );
                 $transformed_query = replace_timestamp( $transformed_query, $shipday );
 
-                $birthday = $this->version->get_birthday( $version->id );
-                $transformed_query = replace_birthday( $transformed_query, $birthday );
-
+                    
                 //  Append the Qb queries and other meta-data into $data
-                $data['query_groups'][$group->tag]['queries'][$query->tag]['title']       = $query->title;
-                $data['query_groups'][$group->tag]['queries'][$query->tag]['query_id']    = $query->id;
-                $data['query_groups'][$group->tag]['queries'][$query->tag]['colour']      = $query->colour;
-                $data['query_groups'][$group->tag]['queries'][$query->tag]['qb_query']    = $transformed_query;
+                $data['query_groups'][$group_tag]['queries'][$query_tag]['title']       = $query_title;
+                $data['query_groups'][$group_tag]['queries'][$query_tag]['query_id']    = $query->id;
+                $data['query_groups'][$group_tag]['queries'][$query_tag]['colour']      = $query->colour;
+                $data['query_groups'][$group_tag]['queries'][$query_tag]['qb_query']    = $transformed_query;
             }
         }
-        // End of retrieving groups and queries by product
-        /*************************************************/
 
-        /******************************************/
-        // Retrieving groups and queries by version
+        /********************************************
+            Retrieving groups and queries by version
+        *********************************************/
         $by_version = array( 
             'entity'    => 'version',
             'entity_id' => $version->id );
