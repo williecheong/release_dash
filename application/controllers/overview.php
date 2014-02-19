b41e1fafa39a7f99fe1ec53d07ec15cc3a0f3c5f
williecheong <cheongwillie@hotmail.com>
2014-02-19 10:41:14 -0800

    Autoload and session config
    Overflow-y hidden on grid titles to prevent layout problem when a
    normal mouse (non-mac) is plugged in.

--- application/controllers/overview.php
+++ application/controllers/overview.php
@@ -4,23 +4,7 @@ class Overview extends CI_Controller {
 
     function __construct() {
         parent::__construct();
-        
-        // Load configs
-        $this->load->database();
-
-        // Load models for playing with local data 
-        $this->load->model('product');
-        $this->load->model('channel');        
-        $this->load->model('version');
-        $this->load->model('group');
-        $this->load->model('query');
-        $this->load->model('cycle');
-        //$this->load->model('comment');
-
-        // Load some helpers for convenience
-        $this->load->helper('url');
-        $this->load->helper('date');
-        $this->load->helper('release_dash');
+        // Autoloaded Config, Helpers, Models 
     }
     
 	public function index() {
