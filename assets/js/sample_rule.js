9d2b19e56f003211b5ca1ecd751c9c4ae10b4a4f
williecheong <cheongwillie@hotmail.com>
2014-02-19 15:06:42 -0800

    Comments added into Javascript files

--- assets/js/sample_rule.js
+++
@@ -1,8 +0,0 @@
-
-    function rule_group_<group_id>() {
-        var main = coreData.query_groups['<group_tag>'].queries['<query_tag;id=1>'].es_data;
-
-        if ( main ) {
-            return 'green';
-        }
-    }
