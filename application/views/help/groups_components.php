d9befffe6d0d732b432ad2e5f640170933d221aa
williecheong <cheongwillie@hotmail.com>
2014-04-02 15:05:47 -0700

    done user guide

---
+++ application/views/help/groups_components.php
@@ -0,0 +1,22 @@
+<div class="row">
+    <div class="row">
+        <div class="col-xs-4">
+            <img class="img-thumbnail" src="/assets/img/help/components_icon.png">
+        </div>
+        <div class="col-xs-4">
+            <img class="img-thumbnail" src="/assets/img/help/components_loading.png">
+        </div>
+        <div class="col-xs-4">
+            <img class="img-thumbnail" src="/assets/img/help/components_display.png">
+        </div>
+    </div>
+    <div class="col-sm-12">
+        
+        <p>
+            Views by component breakdown are available for all groups with only one query, not including reference queries. To access the view, hover over a target group and click on the breakdown icon as shown in the first image above. This will open a modal with a view as shown in the second image. Data about each product component is now being loaded from Elastic Search. A wait of about 30 seconds can be expected for this to be completed.
+        </p>
+        <p>
+            After data about each product component has been completely loaded, the chomping image will be replaced by a "READY" message. Click on any data point on the graph and a table showing the bug count for each component on that date will appear. Note that the list of components for breaking down into can be updated through using the "Update components" link in the navigation menu.
+        </p>
+    </div>
+</div>
