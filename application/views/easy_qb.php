01a1066c63b6142219bcc4ac0f919ed91c24ed46
williecheong <cheongwillie@hotmail.com>
2014-04-08 16:50:46 -0700

    abstracting bzsearch parser into qb_maker.js

--- application/views/easy_qb.php
+++ application/views/easy_qb.php
@@ -1,7 +1,8 @@
 <?php 
     $include = array( 
         'top'    => '<link rel="stylesheet" href="/assets/css/easy_qb.css">',
-        'bottom' => '<script src="/assets/js/easy_qb.js"></script>'
+        'bottom' => '<script src="/assets/js/qb_maker.js"></script>
+                     <script src="/assets/js/easy_qb.js"></script>'
     );
 
     $this->load->view('templates/header', $include);
