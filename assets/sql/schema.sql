4483ea578b9cd235d445536d0b4217eb230b7d5c
williecheong <cheongwillie@hotmail.com>
2014-04-16 15:45:39 -0700

    ci_sessions

--- assets/sql/schema.sql
+++ assets/sql/schema.sql
@@ -1,3 +1,13 @@
+CREATE TABLE IF NOT EXISTS  `ci_sessions` (
+    session_id varchar(40) DEFAULT '0' NOT NULL,
+    ip_address varchar(45) DEFAULT '0' NOT NULL,
+    user_agent varchar(120) NOT NULL,
+    last_activity int(10) unsigned DEFAULT 0 NOT NULL,
+    user_data text NOT NULL,
+    PRIMARY KEY (session_id),
+    KEY `last_activity_idx` (`last_activity`)
+);
+
 CREATE TABLE `administrator` (
     `id` int(11) not null auto_increment,
     `email` varchar(255) not null,
