8b22e91abd71c6be65228ceaf042863599abaf97
williecheong <cheongwillie@hotmail.com>
2014-04-24 16:26:21 -0700

    unit testing CRUD functions in version model
    also removing the need to use database for ci_sessions

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -20,33 +20,82 @@ class Test extends CI_Controller {
             )
         );
 
-        $this->_test_version_models();
-        $this->_test_helpers();
+        // Stores the newly created entries in the db for using and eventually deleting
+        // Array is a set of key value pairs for "entity" => "id" e.g. "product" => "4"
+        $this->test_entries = array();
+
+        try {
+            $this->_test_version_models();;           
+        } catch (Exception $e) {
+            $this->_handle_exceptions($e);
+        }
+        
+        try {
+            $this->_test_helpers();
+        } catch (Exception $e) {            
+            $this->_handle_exceptions($e);
+        }
+
+        // Put at the very end to make sure we delete everything
+        // Removes all of the garbage test data that we just created for testing with
+        $this->_delete_creations();
 
         echo "<style>th{width:20%;}</style>";
         echo $this->unit->report();
         return;
     }
 
-    private function _test_version_models() {
-        $versions = $this->version->retrieve();
-        if ( count($versions) > 0 ) {
-            $title = 'Model: $this->version->retrieve()';
-                $notes = 'Testing to see if we have an array right after retrieving versions.';
-                    $this->unit->run( $versions, 'is_array', $title, $notes);
-                $notes = 'Testing to see if we have an array of objects after retrieving versions.';    
-                    $this->unit->run( $versions[0], 'is_object', $title, $notes);
-            
-            $this->var_version = $versions[0];
-        } else {
-            $title = 'Model: $this->version->retrieve()';
-                $notes = 'No versions stored in the database yet, although highly unlikely.';
-                    $this->unit->run( $versions, array(), $title, $notes);
-            
-            echo "No versions currently stored in the database.";
-            die();
-        }
+    private function _test_version_models() { // Follows CRUD (Create Retrieve Update Delete)
+        $this->test_entries['version'] = $this->version->create(
+            array(
+                'tag' => 'test_tag',
+                'product_id' => '1',
+                'title' => 'Test Title for Version'
+            )
+        ); 
 
+        $versions = $this->version->retrieve(
+            array(
+                'id' => $this->test_entries['version']
+            )
+        );
+
+        $title = 'Model: $this->version->create()';
+        $notes = 'Testing to see if we have an array of 1 object right after retrieving the created version.';
+        $this->unit->run( count($versions), 1, $title, $notes);
+
+        $title = 'Model: $this->version->retrieve()';
+        $notes = 'Testing to see if we have an array right after retrieving versions.';
+        $this->unit->run( $versions, 'is_array', $title, $notes);
+        $notes = 'Testing to see if we have an array of objects after retrieving versions.';    
+        $this->unit->run( $versions[0], 'is_object', $title, $notes);
+
+        $this->var_version = $versions[0];
+
+        $title = 'Model: $this->version->retrieve()';
+        $notes = 'Testing to see if we actually stored the right values in the test version.';
+        $this->unit->run( $this->var_version->tag, 'test_tag', $title, $notes);
+        
+        return;
+    }
+
+    private function _handle_exceptions( $e ) {
+        echo 'Caught exception: ',  $e->getMessage(), "\n";
+    }
+
+    private function _delete_creations() {
+        foreach ($this->test_entries as $entity => $id) {
+            try {
+                $this->{$entity}->delete(
+                    array(
+                        'id' => $id
+                    )
+                );
+                echo 'Successfully deleted test <strong>'.$entity.'</strong>: ID = '.$id."\n";
+            } catch (Exception $e) {
+                echo 'IMPORTANT: Failed to delete test entry from <strong>' . $entity . '</strong> table (ID = '.$id.')';
+            }
+        }
         return;
     }
 
